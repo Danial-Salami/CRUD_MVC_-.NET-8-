9ee439832d51b7eae5d222ad3609d8cf015c9128
Danial Salami <81363393+Danial-Salami@users.noreply.github.com>
2023-12-09 14:44:18 +0330

    Update Program.cs

--- E_commerceWeb/Program.cs
+++ E_commerceWeb/Program.cs
@@ -2,7 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+//this is 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
