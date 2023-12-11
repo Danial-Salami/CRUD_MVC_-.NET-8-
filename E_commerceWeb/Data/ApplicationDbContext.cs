8f5cb230201dc4789b4dcd32a44455af336b132e
Danial0021 <danialsari13@gmail.com>
2023-12-11 17:38:59 +0330

    add category to migration and data and model

---
+++ E_commerceWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using E_commerceWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace E_commerceWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        
+        public DbSet<Category> Categories { get; set; }
+    }
+}
---
+++ E_commerceWeb/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace E_commerceWeb.Models
+{
+    public class Category
+    {
+
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+       
+
+
+    }
+}
